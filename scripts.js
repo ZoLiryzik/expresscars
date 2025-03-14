3aa07c196197601d63b73f9a3e0de0e151a811ec
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-14 21:06:05 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     const servicesContainer = document.getElementById("services");
-    const services = JSON.parse(servicesData);
 
-    services.forEach(service => {
-        const serviceCard = document.createElement("div");
-        serviceCard.classList.add("card");
+    fetch('data.json')
+        .then(response => response.json())
+        .then(services => {
+            services.forEach(service => {
+                const serviceCard = document.createElement("div");
+                serviceCard.classList.add("card");
 
-        serviceCard.innerHTML = `
-            <div class="card__poster" style="background-image: url(${service.image});"></div>
-            <div class="card__databox">
-                <h2 class="card-databox__heading">${service.title}</h2>
-                <p class="card-databox__description">${service.description}</p>
-                <button class="card-databox__read-more-btn" onclick="openModal(${service.id})">Подробнее</button>
-            </div>
-        `;
+                serviceCard.innerHTML = `
+                    <div class="card__poster" style="background-image: url(${service.image});"></div>
+                    <div class="card__databox">
+                        <h2 class="card-databox__heading">${service.title}</h2>
+                        <p class="card-databox__description">${service.description}</p>
+                        <button class="card-databox__read-more-btn" onclick="openModal(${service.id})">Подробнее</button>
+                    </div>
+                `;
 
-        servicesContainer.appendChild(serviceCard);
-    });
+                servicesContainer.appendChild(serviceCard);
+            });
+        });
 });
 
 function openModal(id) {
